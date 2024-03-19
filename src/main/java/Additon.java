439c58b9f31c439dd1587aa9d5a23146caac602f
Deepika Ginjupalli <nagadeepikaginjupalli@gmail.com>
2024-03-19 14:41:42 +0530

    Submitting the code.

---
+++ src/main/java/Additon.java
@@ -0,0 +1,10 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Addition{
+public void add() {
+
+short a=10, b=20, c=30, d=40, e=50, f=60, g=70, h=80, i=90;
+int result=a+b+c+d+e+f+g+h+i;
+System.out.println(result);
+}
+}
